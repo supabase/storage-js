4471b288cb9bf9ce9d009cd79fd321a91fcc23f8
fenos <fabri.feno@gmail.com>
2022-09-21 12:57:46 +0100

    chore: increased code coverage

--- test/storageApi.test.ts
+++ test/storageApi.test.ts
@@ -8,55 +8,56 @@ const KEY =
 const storage = new StorageBucketApi(URL, { Authorization: `Bearer ${KEY}` })
 const newBucketName = `my-new-bucket-${Date.now()}`
 
-test('Build to succeed', async () => {
-  // Basic test to ensure TS build is working.
-  expect(true).toEqual(true)
-})
+describe('bucket api', () => {
+  test('Build to succeed', async () => {
+    // Basic test to ensure TS build is working.
+    expect(true).toEqual(true)
+  })
 
-test('Get all buckets', async () => {
-  const res = await storage.listBuckets()
-  expect(res.data).not.toBeNull()
-  expect(res.data).toMatchSnapshot()
-})
+  test('Get all buckets', async () => {
+    const res = await storage.listBuckets()
+    expect(res.data).not.toBeNull()
+  })
 
-test('Get bucket by id', async () => {
-  const res = await storage.getBucket('bucket2')
-  expect(res.data).toMatchSnapshot()
-})
+  test('Get bucket by id', async () => {
+    const res = await storage.getBucket('bucket2')
+    expect(res.data).toMatchSnapshot()
+  })
 
-test('Get bucket with wrong id', async () => {
-  const res = await storage.getBucket('not-exist-id')
-  expect(res.error).toMatchSnapshot()
-})
+  test('Get bucket with wrong id', async () => {
+    const res = await storage.getBucket('not-exist-id')
+    expect(res.error).toMatchSnapshot()
+  })
 
-test('create new bucket', async () => {
-  const res = await storage.createBucket(newBucketName)
-  expect(res.data).toEqual(newBucketName)
-})
+  test('create new bucket', async () => {
+    const res = await storage.createBucket(newBucketName)
+    expect(res.data).toEqual(newBucketName)
+  })
 
-test('create new public bucket', async () => {
-  const newPublicBucketName = 'my-new-public-bucket'
-  await storage.createBucket(newPublicBucketName, { public: true })
-  const res = await storage.getBucket(newPublicBucketName)
-  expect(res.data!.public).toBe(true)
-})
+  test('create new public bucket', async () => {
+    const newPublicBucketName = 'my-new-public-bucket'
+    await storage.createBucket(newPublicBucketName, { public: true })
+    const res = await storage.getBucket(newPublicBucketName)
+    expect(res.data!.public).toBe(true)
+  })
 
-test('update bucket', async () => {
-  const updateRes = await storage.updateBucket(newBucketName, { public: true })
-  expect(updateRes.error).toBeNull()
-  expect(updateRes.data).toMatchSnapshot()
-  const getRes = await storage.getBucket(newBucketName)
-  expect(getRes.data!.public).toBe(true)
-})
+  test('update bucket', async () => {
+    const updateRes = await storage.updateBucket(newBucketName, { public: true })
+    expect(updateRes.error).toBeNull()
+    expect(updateRes.data).toMatchSnapshot()
+    const getRes = await storage.getBucket(newBucketName)
+    expect(getRes.data!.public).toBe(true)
+  })
 
-test('empty bucket', async () => {
-  const res = await storage.emptyBucket(newBucketName)
-  expect(res.error).toBeNull()
-  expect(res.data).toMatchSnapshot()
-})
+  test('empty bucket', async () => {
+    const res = await storage.emptyBucket(newBucketName)
+    expect(res.error).toBeNull()
+    expect(res.data).toMatchSnapshot()
+  })
 
-test('delete bucket', async () => {
-  const res = await storage.deleteBucket(newBucketName)
-  expect(res.error).toBeNull()
-  expect(res.data).toMatchSnapshot()
+  test('delete bucket', async () => {
+    const res = await storage.deleteBucket(newBucketName)
+    expect(res.error).toBeNull()
+    expect(res.data).toMatchSnapshot()
+  })
 })
