supabase/storage-js

commit 84362213b563e801aba017225c2b46fdeb47b78b

fix: Properly handle errors in Upload Api  (related to StorageError) (#216)
* fix upload error handling

* bypass type check

* fix _getRequestParams

src/lib/fetch.ts
@@ -45,15 +45,17 @@ const _getRequestParams = (
 ) => {
   const params: { [k: string]: any } = { method, headers: options?.headers || {} }
 
-  if (method === 'GET') {
+  if (method === 'GET' || !body) {
     return params
   }
 
-  params.headers = { 'Content-Type': 'application/json', ...options?.headers }
-
-  if (body) {
+  if (body instanceof FormData) {
+    params.body = body
+  } else {
+    params.headers = { 'Content-Type': 'application/json', ...options?.headers }
     params.body = JSON.stringify(body)
   }
+
   return { ...params, ...parameters }
 }
 

src/packages/StorageFileApi.ts
@@ -1,5 +1,5 @@
 import { isStorageError, StorageError, StorageUnknownError } from '../lib/errors'
-import { Fetch, get, head, post, remove } from '../lib/fetch'
+import { Fetch, get, head, post, put, remove } from '../lib/fetch'
 import { recursiveToCamel, resolveFetch } from '../lib/helpers'
 import {
   FileObject,
@@ -118,23 +118,16 @@ export default class StorageFileApi {
 
       const cleanPath = this._removeEmptyFolders(path)
       const _path = this._getFinalPath(cleanPath)
-      const res = await this.fetch(`${this.url}/object/${_path}`, {
-        method,
-        body: body as BodyInit,
-        headers,
-        ...(options?.duplex ? { duplex: options.duplex } : {}),
-      })
+      const data = await (method == 'PUT' ? put : post)(
+        this.fetch,
+        `${this.url}/object/${_path}`,
+        body as object,
+        { headers, ...(options?.duplex ? { duplex: options.duplex } : {}) }
+      )
 
-      const data = await res.json()
-
-      if (res.ok) {
-        return {
-          data: { path: cleanPath, id: data.Id, fullPath: data.Key },
-          error: null,
-        }
-      } else {
-        const error = data
-        return { data: null, error }
+      return {
+        data: { path: cleanPath, id: data.Id, fullPath: data.Key },
+        error: null,
       }
     } catch (error) {
       if (isStorageError(error)) {
@@ -207,22 +200,16 @@ export default class StorageFileApi {
         headers['content-type'] = options.contentType as string
       }
 
-      const res = await this.fetch(url.toString(), {
-        method: 'PUT',
-        body: body as BodyInit,
-        headers,
-      })
+      const data = await put(
+        this.fetch,
+        url.toString(),
+        body as object,
+        { headers }
+      )
 
-      const data = await res.json()
-
-      if (res.ok) {
-        return {
-          data: { path: cleanPath, fullPath: data.Key },
-          error: null,
-        }
-      } else {
-        const error = data
-        return { data: null, error }
+      return {
+        data: { path: cleanPath, fullPath: data.Key },
+        error: null,
       }
     } catch (error) {
       if (isStorageError(error)) {

commit 68173ee0c7138c576345145c0b88f8e66493ac1c

fix: resolve regression with uploading files introduced by #216

src/lib/fetch.ts
@@ -1,5 +1,5 @@
 import { StorageApiError, StorageUnknownError } from './errors'
-import { resolveResponse } from './helpers'
+import { isPlainObject, resolveResponse } from './helpers'
 import { FetchParameters } from './types'
 
 export type Fetch = typeof fetch
@@ -49,11 +49,11 @@ const _getRequestParams = (
     return params
   }
 
-  if (body instanceof FormData) {
-    params.body = body
-  } else {
+  if (isPlainObject(body)) {
     params.headers = { 'Content-Type': 'application/json', ...options?.headers }
     params.body = JSON.stringify(body)
+  } else {
+    params.body = body
   }
 
   return { ...params, ...parameters }

src/lib/helpers.ts
@@ -37,3 +37,23 @@ export const recursiveToCamel = (item: Record<string, any>): unknown => {
 
   return result
 }
+
+/**
+ * Determine if input is a plain object
+ * An object is plain if it's created by either {}, new Object(), or Object.create(null)
+ * source: https://github.com/sindresorhus/is-plain-obj
+ */
+export const isPlainObject = (value: object): boolean => {
+  if (typeof value !== 'object' || value === null) {
+    return false
+  }
+
+  const prototype = Object.getPrototypeOf(value)
+  return (
+    (prototype === null ||
+      prototype === Object.prototype ||
+      Object.getPrototypeOf(prototype) === null) &&
+    !(Symbol.toStringTag in value) &&
+    !(Symbol.iterator in value)
+  )
+}

test/storageFileApi.test.ts
@@ -6,7 +6,7 @@ import FormData from 'form-data'
 import assert from 'assert'
 // @ts-ignore
 import fetch, { Response } from '@supabase/node-fetch'
-import { StorageError } from '../src/lib/errors'
+import { StorageApiError, StorageError } from '../src/lib/errors'
 
 // TODO: need to setup storage-api server for this test
 const URL = 'http://localhost:8000/storage/v1'
@@ -202,11 +202,10 @@ describe('Object API', () => {
       })
 
       const res = await storage.from(bucketName).upload(uploadPath, file)
-      expect(res.error).toEqual({
-        error: 'Payload too large',
-        message: 'The object exceeded the maximum allowed size',
-        statusCode: '413',
-      })
+
+      const outError = res.error as StorageApiError
+      expect(outError).toBeInstanceOf(StorageApiError)
+      expect(outError.message).toBe('The object exceeded the maximum allowed size')
     })
 
     test('can upload a file with a valid mime type', async () => {
@@ -232,11 +231,9 @@ describe('Object API', () => {
       const res = await storage.from(bucketName).upload(uploadPath, file, {
         contentType: 'image/jpeg',
       })
-      expect(res.error).toEqual({
-        error: 'invalid_mime_type',
-        message: 'mime type image/jpeg is not supported',
-        statusCode: '415',
-      })
+      const outError = res.error as StorageApiError
+      expect(outError).toBeInstanceOf(StorageApiError)
+      expect(outError.message).toBe('mime type image/jpeg is not supported')
     })
 
     test('sign url for upload', async () => {
@@ -299,11 +296,9 @@ describe('Object API', () => {
         .from(bucketName)
         .uploadToSignedUrl(data.path, data.token, file)
 
-      expect(uploadRes2.error).toEqual({
-        error: 'Duplicate',
-        message: 'The resource already exists',
-        statusCode: '409',
-      })
+      const outError = uploadRes2.error as StorageApiError
+      expect(outError).toBeInstanceOf(StorageApiError)
+      expect(outError.message).toBe('The resource already exists')
     })
   })
 